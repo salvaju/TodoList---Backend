dee29da1f78fb206604118c92877597b5c11d422
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-19 14:20:32 +0800

    [TodoListBackend][Juan Carlos Salvador] feat: implement seperate class for CorsGlobalConfiguration

---
+++ src/main/java/com/example/todolist/config/CorsGlobalConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.todolist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class CorsGlobalConfiguration {
+
+    @Bean
+    WebMvcConfigurer corsConfigurer(){
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+
+    }
+
+}
